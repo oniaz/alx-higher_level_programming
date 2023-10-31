ca78bd4f4f2b907049f31a3fddc89f11952a269a
“oniaz” <32391049+oniaz@users.noreply.github.com>
2023-10-31 16:10:15 -0700

    no newline after printing the digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,5 @@ def print_last_digit(number):
         digit = ((number * -1) % 10) * -1
     else:
         digit = number % 10
-    print("{}".format(digit))
+    print("{}".format(digit), end="")
     return digit
