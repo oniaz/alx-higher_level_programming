aa6b108ba1fb29648f4be269ddfd4a9cae27a8be
“oniaz” <32391049+oniaz@users.noreply.github.com>
2023-10-31 16:14:35 -0700

    I would've done it this way from the beginning if it wasn't for task 1

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        digit = ((number * -1) % 10) * -1
-    else:
+        number *= -1
         digit = number % 10
     print("{}".format(digit), end="")
     return digit
