c8799597c6cfb8cd784da59ae5fe1d043917b5e4
“oniaz” <32391049+oniaz@users.noreply.github.com>
2023-10-31 19:16:59 -0700

    didn't make this a fucntion 🤡

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if (i % 3 == 0):
-        print("Fizz", end="")
-    if (i % 5 == 0):
-        print("Buzz", end="")
-    if (i % 5 != 0) and (i % 3 != 0):
-        print(f"{i}", end="")
-    print("", end=" ")
-print("")
+def fizzbuzz():
+    for i in range(1, 101):
+        if (i % 3 == 0):
+            print("Fizz", end="")
+        if (i % 5 == 0):
+            print("Buzz", end="")
+        if (i % 5 != 0) and (i % 3 != 0):
+            print(f"{i}", end="")
+        print("", end=" ")
+    print("")
