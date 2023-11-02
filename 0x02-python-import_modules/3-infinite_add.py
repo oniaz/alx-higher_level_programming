d4477e101d18348cd1697260102105cf9c537d92
oniaz <32391049+oniaz@users.noreply.github.com>
2023-11-03 00:38:56 +0200

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    sum = 0
+    length = len(argv) - 1
+    if length:
+        for i in range(length):
+            sum += int(argv[i+1])
+    print(sum)
