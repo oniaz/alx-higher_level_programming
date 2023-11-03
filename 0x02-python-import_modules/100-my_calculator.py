75e895117a0e6ca280e7caeba44ecf7d73f10a0d
oniaz <32391049+oniaz@users.noreply.github.com>
2023-11-03 02:31:26 +0200

    adv task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+if __name__ == '__main__':
+    from sys import argv, exit
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    op = argv[2]
+    if op not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if op == '+':
+        result = add(a, b)
+    if op == '-':
+        result = sub(a, b)
+    if op == '*':
+        result = mul(a, b)
+    if op == '/':
+        result = div(a, b)
+    print(f"{a} {op} {b} = {result}")
