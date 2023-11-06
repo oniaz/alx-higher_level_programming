3bc40567210e896fb98035caee6e253011ac6a43
oniaz <32391049+oniaz@users.noreply.github.com>
2023-11-06 08:04:49 +0200

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is not None:
+        for row in matrix:
+            for i in row:
+                print("{}".format(i), end="")
+                if i != row[-1]:
+                    print(" ", end="")
+            print("")
