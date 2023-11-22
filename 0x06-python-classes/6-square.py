22797f7dfa633009e7423790370266f6e5326ddd
oniaz <32391049+oniaz@users.noreply.github.com>
2023-11-22 04:55:10 +0200

    edited the exception condition for position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,7 +36,11 @@ class Square:
     @position.setter
     def position(self, value):
         """ sets le square size """
-        if not isinstance(value, tuple) or not len(value) == 2:
+        if not (isinstance(value, tuple) and
+                len(value) != 2 and
+                not (isinstance(value[0], int)) and
+                not (isinstance(value[1], int)) and
+                value[0] > 0 and value[1] > 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = value
 
