61d62192cd104a4ff46295173994c29bc65e6353
oniaz <32391049+oniaz@users.noreply.github.com>
2023-11-22 03:41:49 +0200

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""task 1"""
+
+
+class Square:
+    """class Square that defines a square.
+
+    Args:
+        size (int): square size"""
+    def __init__(self, size=None):
+        self.size = size
