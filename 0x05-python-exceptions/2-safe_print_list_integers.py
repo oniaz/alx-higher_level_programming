219df27333db8795d0300f8512a182945000901a
oniaz <32391049+oniaz@users.noreply.github.com>
2023-11-26 06:01:03 +0200

    task 2 - so this week i took off i spent it watching tv series. i didn't think this would happend but I got hooked on upload it's a good show. i like the humour. i only watched it because Robbie Amell and didn't have much expectations for it but i'm impressed

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (TypeError, ValueError):
+            pass
+        else:
+            c += 1
+    print()
+    return c
