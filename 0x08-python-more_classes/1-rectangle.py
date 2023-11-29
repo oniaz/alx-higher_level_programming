7d184cccaf2516f6e32a199808c8d988475e976b
oniaz <32391049+oniaz@users.noreply.github.com>
2023-11-29 05:14:33 +0200

    figured out the right way to use setter in init

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""A module representing a rectangle """
+
+
+class Rectangle:
+    """A class representing a rectangle """
+
+    def __init__(self, width=0, height=0):
+        """Initialize a rectangle with given width and height.
+
+        Args:
+            size (width): The width of the rectangle.
+            size (height): The height of the rectangle.
+
+        """
+        if width:
+            self.width = width
+        if width:
+            self.height = height
+
+    @property
+    def width(self):
+        """Retrieves the width of the rectangle.
+
+        Returns:
+            int: the width of the rectangle.
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """Retrieves the height of the rectangle.
+
+        Returns:
+            int: the height of the rectangle.
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle.
+
+        Args:
+        - value (int): The value to be set as the width of the rectangle.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle.
+
+        Args:
+        - value (int): The value to be set as the height of the rectangle.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
