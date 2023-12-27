68056ab642f7b7cf7d24d594426b4ebbf36350f6
oniaz <32391049+oniaz@users.noreply.github.com>
2023-12-27 07:28:33 +0200

    i ahte all of them btihces from svool aghjhg gtfo

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Task 6 module. """
+
+
+class BaseGeometry():
+    """ Base Class with an as-for-now a nonfunctioning area function."""
+    def area(self):
+        """ Raises an error message """
+        raise Exception("area() is not implemented")
