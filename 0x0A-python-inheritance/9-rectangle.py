d882256cc330a6d302d608896e0b626c4bc0bb04
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-01 19:05:23 +0200

    just the area. maybe

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Task 8 module. """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ inheits from basegeomnety. boi i dont want want to documnet """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        return self.__width * self.__height
