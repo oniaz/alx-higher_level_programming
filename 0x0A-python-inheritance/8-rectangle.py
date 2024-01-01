9351d576b66a668421dc93cc5778866f16600766
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-01 18:56:23 +0200

    eheh

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,11 +6,6 @@ class BaseGeometry():
     """ Base Class with an as-for-now a nonfunctioning area function.
         and an int validator function.
     """
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
 
     def area(self):
         """ Raises an error message """
@@ -22,3 +17,13 @@ class BaseGeometry():
             raise TypeError(name + " must be an integer")
         if (value < 1):
             raise ValueError(name + " must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ inheits from basegeomnety. boi i dont want want to documnet """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
