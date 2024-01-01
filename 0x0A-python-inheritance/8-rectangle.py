b4dd73f6a5fc4bbf305a8ebdc2699d46ed4a0db2
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-01 18:52:13 +0200

    pushing before the power goes out

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Task 8 module. """
+
+
+class BaseGeometry():
+    """ Base Class with an as-for-now a nonfunctioning area function.
+        and an int validator function.
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ Raises an error message """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Checks if the value is a positive int """
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if (value < 1):
+            raise ValueError(name + " must be greater than 0")
