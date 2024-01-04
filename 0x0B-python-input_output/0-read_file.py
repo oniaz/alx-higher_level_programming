14e19539ada4a701442162ba5f58b3c325011028
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-04 23:55:00 +0200

    oh well heh.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
             filename (str): The path to the file to be read.
     """
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
