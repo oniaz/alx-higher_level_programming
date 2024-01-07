dc3b82715d2bad67302ae1775cc29d216d0e8855
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-07 21:08:03 +0200

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" task 1 - Base Class"""
+
+class Base:
+    " Base Class. maganes id attribute for all subclasses"
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initializing the base instance by assigning an id
+
+            Args:
+                id (int): the instance id.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.self.id = Base.__nb_objects
