905f08ed2091331bb6a2ee313f10c0bfeefa9d7e
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-09 06:59:39 +0200

    getting the dict for each of the objects in the list (expanding the objects), then saving the list after serialization to a json file with the class name.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Returns the JSON string representation of the passed dictionary."""
@@ -28,3 +28,19 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes the JSON string representation of the given list object to
+            a json file.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                thelist = []
+                for obj in list_objs:
+                    thelist.append(cls.to_dictionary(obj))
+                file.write(Base.to_json_string(thelist))
+                # json.dump(thelist, file)
