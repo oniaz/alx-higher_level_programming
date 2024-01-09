6d61699240f853f61c1d03fc57f52b06c26ea495
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-09 02:06:47 +0200

    square 1

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A module representing a square. """
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Square class. A subclass of Rectangle class. """
+
+    def __init__(self, size=0, x=0, y=0, id=None):
+        """ initializing the square instance.
+
+            Args:
+                size (int): the length of a side of the square.
+                x (int): x coordinate.
+                y (int): y coordinate.
+                id (int): the instance id.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Returns a string with the square attributes values. """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
