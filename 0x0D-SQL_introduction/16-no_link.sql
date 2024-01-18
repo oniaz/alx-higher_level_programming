80b95800d065f1eeff9c0b179d5de0d3eeff7fc7
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-18 12:09:37 +0200

    docs

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,1 +1,2 @@
+-- lists score and name for all records of the table second_table where name value isn't null, sorted by descending order. 
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
