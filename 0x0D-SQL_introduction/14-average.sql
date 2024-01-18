3bb792030b4b2a4bcc1f40a882a60e63b45b3907
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-18 11:26:01 +0200

    named the result column "average"

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,3 @@
---  computes the score average of all records in the table second_table
-SELECT AVG(score) FROM second_table
+--  computes the score average of all records in the table "second_table".
+--- The naem of the result column "average".
+SELECT AVG(score) AS average FROM second_table
