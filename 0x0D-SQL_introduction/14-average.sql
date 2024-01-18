368648b818009efd145ac6fb97766e76489c20c1
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-18 11:37:32 +0200

    syntax issues

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 --  computes the score average of all records in the table "second_table".
---- The naem of the result column "average".
-SELECT AVG(score) AS average FROM second_table
+-- The name of the result column "average".
+SELECT AVG(score) AS average FROM second_table;
