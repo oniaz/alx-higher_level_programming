6cd90dbb9fa5823516cb103588d1299e639888b6
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-12 02:34:54 +0200

    task 1 - creating a user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- task 0
+-- createing the MySQL server user "user_0d_1" granted all privileges with password set to "user_0d_1_pwd"
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
