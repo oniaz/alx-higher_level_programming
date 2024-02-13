4b280155d8672fcad76b3354bccd0a29e0ff8b6c
“oniaz” <32391049+oniaz@users.noreply.github.com>
2024-02-13 15:19:54 -0800

    task6 - arrays

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arr = [];
+arr[0] = 'C is fun';
+arr[1] = 'Python is cool';
+arr[2] = 'JavaScript is amazing';
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
