714b137623fef8f6df64dbac0a7d58848eb42559
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-16 05:41:12 +0200

    task 8 - Query cities by state  without using JOIN

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- task 8
+-- Lists the cities of California and their ids found in the database 'hbtn_0d_usa'.
+
+USE hbtn_0d_usa;
+
+SELECT id, name
+FROM   cities
+WHERE  state_id = (SELECT id
+                   FROM   states
+                   WHERE  name = "California")
+ORDER BY id ASC; 
