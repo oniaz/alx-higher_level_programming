3a762b2ab64040374cd8ce7a4b4b5269478a82e2
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-16 05:09:03 +0200

    task 7 - foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+-- task 6
+-- Creating the database 'hbtn_0d_usa' with table 'cities'
+-- 'cities' table has the fields:
+--      'id' - INT unique, auto generated, not null and is a primary key.
+--      'state_id' - INT, not null, is a FOREIGN KEY that references to id of the 'states' table.
+--      'name' - VARCHAR(256) not null.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
