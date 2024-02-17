5640c6c238187a094a29930d687a0fd8f4393eff
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-17 04:44:41 +0200

    task 16 - RIGHT JOIN , displaying all shows and their genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,10 +1,10 @@
 -- task 16
--- Listing all shows and their genres.
+-- Listing all shows and their genres if exists.
 
 SELECT tv_shows.title, tv_genres.name
 FROM   tv_show_genres
        JOIN tv_genres
          ON tv_show_genres.genre_id = tv_genres.id
-       JOIN tv_shows
+       RIGHT JOIN tv_shows
          ON tv_show_genres.show_id = tv_shows.id
 ORDER  BY tv_shows.title ASC, tv_genres.name ASC; 
