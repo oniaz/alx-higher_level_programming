63e4bbe0248dd1d288b3d52fdc2813cc06db1f77
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-17 04:18:55 +0200

    task 14 - joining three tables

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,11 @@
+-- task 14
+-- Listing all the genres for the show 'Dexter'
+
+SELECT tv_genres.name
+FROM   tv_show_genres
+       JOIN tv_genres
+         ON tv_show_genres.genre_id = tv_genres.id
+       JOIN tv_shows
+         ON tv_show_genres.show_id = tv_shows.id
+WHERE  tv_shows.title = 'Dexter'
+ORDER  BY tv_genres.name ASC; 
