964c033c606102ab2b1efacb848f16698c09cff5
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-17 04:36:03 +0200

    task 15 -3 tables for komédé

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- task 15
+-- Listing all the Comedy shows.
+
+SELECT tv_shows.title
+FROM   tv_show_genres
+       JOIN tv_genres
+         ON tv_show_genres.genre_id = tv_genres.id
+       JOIN tv_shows
+         ON tv_show_genres.show_id = tv_shows.id
+WHERE  tv_genres.name = 'Comedy'
+ORDER  BY tv_shows.title ASC; 
