dd758cef70533b4a05f6fd7b8e077815fa49a520
“oniaz” <32391049+oniaz@users.noreply.github.com>
2024-02-20 21:17:34 -0800

    task 4 - join 2 tables

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""listing all cities and their states"""
+import MySQLdb
+
+if __name__ == "__main__":
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,  # Specify the port here
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cur = db.cursor()
+    cur.execute(
+            "SELECT cities.id, cities.name, states.name FROM cities " +
+            "JOIN states ON cities.state_id = states.id ORDER BY cities.id"
+            )
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
