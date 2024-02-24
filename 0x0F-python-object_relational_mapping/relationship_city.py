25912cd9ed96ec76fcfe64522efb7a00cc5826c9
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-24 04:25:15 +0200

    task 100 part 1 -     Imporve the models by defining the relationship between State and City classes

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""City Module
+    Imporved by defining the relationship between State and City classes"""
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+
+
+class City(Base):
+    """City Class"""
+
+    __tablename__ = 'cities'
+    id = Column(
+            Integer,
+            autoincrement=True,
+            unique=True,
+            nullable=False,
+            primary_key=True)
+
+    name = Column(
+            String(128),
+            nullable=False)
+
+    state_id = Column(
+           Integer,
+           ForeignKey('states.id', ondelete="CASCADE"),
+           nullable=False)
+
+    state = relationship("State", back_populates="cities")
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""State Module
+    Imporved by defining the relationship between State and City classes"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State Class"""
+    __tablename__ = 'states'
+    id = Column(
+            Integer,
+            autoincrement=True,
+            unique=True,
+            nullable=False,
+            primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", cascade="all, delete", back_populates="state")
