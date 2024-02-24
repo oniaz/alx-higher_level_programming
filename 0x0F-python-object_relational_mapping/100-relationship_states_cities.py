7c0b04e04217adfedd329a608d9ff8e21929fded
oniaz <32391049+oniaz@users.noreply.github.com>
2024-02-24 04:35:05 +0200

    task 100 part 2 - Create the state 'California' with the City 'San Francisco'

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+"""Createing the state 'California' with the City 'San Francisco'."""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    URI = 'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2],
+                                                      argv[3])
+    engine = create_engine(URI, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="California")
+    new_city = City(name="San Francisco")
+
+    new_state.cities.append(new_city)
+    session.add(new_state)
+
+    session.commit()
+    session.close()
