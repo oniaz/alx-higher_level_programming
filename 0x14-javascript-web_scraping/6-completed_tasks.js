5226b28c0ee3b94a4016272939ce40f40e1e6af4
“oniaz” <32391049+oniaz@users.noreply.github.com>
2024-03-26 22:31:06 -0700

    computing information from data extracted through an API

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const jsonData = JSON.parse(body);
+  const result = {};
+
+  for (let i = 0; i < jsonData.length; i++) {
+    const task = jsonData[i];
+    if (!result[task.userId]) {
+      result[task.userId] = 0;
+    }
+    if (task.completed) {
+      result[task.userId] += 1;
+    }
+  }
+  console.log(result);
+});
