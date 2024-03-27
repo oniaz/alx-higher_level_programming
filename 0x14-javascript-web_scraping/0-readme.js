10cda8f25265689cd9e9b40dbea5386ed361a4ff
“oniaz” <32391049+oniaz@users.noreply.github.com>
2024-03-26 17:55:25 -0700

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+  console.log(data);
+});
