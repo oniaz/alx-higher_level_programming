fbc2e1f085c900c83b867eb7419d332602529620
oniaz <32391049+oniaz@users.noreply.github.com>
2024-04-04 02:16:20 +0200

    no shebangs

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,3 @@
-#!/usr/bin/node
-
 // updates the text color of the <header> element to red (#FF0000):
 const headerElement = document.querySelector('header');
 headerElement.style.color = '#FF0000';
--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,7 +1,4 @@
-#!/usr/bin/node
-
 // updates the text color of the <header> element to red (#FF0000):
-
 $(document).ready(function() {
     $('header').css('color', '#FF0000');
   });
