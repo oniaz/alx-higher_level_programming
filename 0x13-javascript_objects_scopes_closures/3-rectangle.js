85aaf51522160bf413fb0c6b4dad168a93a97078
oniaz <32391049+oniaz@users.noreply.github.com>
2024-05-24 04:41:51 +0300

    task 3 - print instance function

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 1 && h > 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let h = 0; h < this.height; h++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
